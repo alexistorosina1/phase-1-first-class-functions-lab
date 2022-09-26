73ff39f1e6a0456920243956787ab06f8a09175c
Alexis Torosina <alexistorosina1@gmail.com>
2022-09-26 13:50:21 -0400

    commit tree 907b46cb8600ef24ac6201ce0f2cc585bfdbde21

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solution in this file!
+const names = ["Antonia", "Nuru", "Amari", "Mo"]
+const returnFirstTwoDrivers = function() {
+    return names.slice(0, 2)
+}
+const returnLastTwoDrivers = function() {
+    return names.slice(2,4);
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+// const number = (num1, num2) => {
+//     return num1 * num2
+// }
+const createFareMultiplier = () => {
+    return function (num1, num2) {
+        return num1 * num2
+    }
+}
